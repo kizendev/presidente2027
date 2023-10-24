7ba256f135769f0200bdcc5e0a3dabc2a6354d89
kevinzanzi <69643444+kevinzanzi@users.noreply.github.com>
2023-10-24 14:34:39 -0300

    Updates

--- src/App.svelte
+++ src/App.svelte
@@ -15,9 +15,9 @@
         >© 2023 Kevin Zanzi.</span
       >
       <span class="text-sm text-gray-400 dark:text-gray-500">
-        <a href="/" class="hover:underline"> Enviar Propuesta </a>
+        <a href="." class="hover:underline"> Enviar Propuesta </a>
         ·
-        <a href="/" class="hover:underline"> Boleta Oficial </a>
+        <a href="." class="hover:underline"> Boleta Oficial </a>
       </span>
       <span class="text-sm text-gray-400 dark:text-gray-500">
         Todas las propuestas fueron generadas por inteligencia artificial y no
