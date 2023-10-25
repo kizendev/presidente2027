86dab25af9892967ad98234106a2794d2cd3f4a5
kevinzanzi <69643444+kevinzanzi@users.noreply.github.com>
2023-10-25 17:20:41 -0300

    aspect ratio for image

--- src/lib/component/HeroImage.svelte
+++ src/lib/component/HeroImage.svelte
@@ -1,5 +1,5 @@
 <img
   src="./heroj.jpg"
   alt="Imagen de la bandera Argentina"
-  class="h-[calc(100vh_-_7rem)] w-full dark:brightness-90"
+  class="h-[calc(100vh_-_7rem)] w-full object-contain dark:brightness-90"
 />
